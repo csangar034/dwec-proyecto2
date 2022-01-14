fb6f5aeefdd825f54872980e46e341aa691e1329
Cristina Sanchez García <cristinasanchezgarcia@pc254-8.11001828.11.andared.ced.junta-andalucia.es>
2022-01-14 16:07:40 +0100

    header y footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Home from './componentes/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'; 
 
+// Fuente: https://blog.nubecolectiva.com/como-crear-una-pagina-web-con-react-16-9-y-bootstrap-4-parte-1/
 ReactDOM.render(
-  "Hola mundo",
+  <Home />, 
   document.getElementById('root')
 );
